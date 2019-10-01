432992518f334a1a2311c43a8a0b1ea5f376a057
Sami Salonen <ssalonen@gmail.com>
2019-10-01 21:39:10 +0300

    working dir fixes

--- ci/script.sh
+++ ci/script.sh
@@ -4,10 +4,10 @@ set -ex
 
 # This is the "test phase", tweak it as you see fit
 main() {
-    # Build custom docker images for hte cross build
-    docker build -t cross-libcec-sys:armv7-unknown-linux-gnueabihf-0.1.16 ../cross_targets/Dockerfile.armv7-unknown-linux-gnueabihf
-    docker build -t cross-libcec-sys:x86_64-unknown-linux-gnu-0.1.16 ../cross_targets/Dockerfile.x86_64-unknown-linux-gnu
-    
+    # Build custom docker images for the cross build
+    docker build -t cross-libcec-sys:armv7-unknown-linux-gnueabihf-0.1.16 cross_targets/Dockerfile.armv7-unknown-linux-gnueabihf
+    docker build -t cross-libcec-sys:x86_64-unknown-linux-gnu-0.1.16 cross_targets/Dockerfile.x86_64-unknown-linux-gnu
+
     cross build --target $TARGET
     cross build --target $TARGET --release
 
