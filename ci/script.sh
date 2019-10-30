4e44ad69d0b0b92e2900cb6a1638f53164f51b06
Sami Salonen <ssalonen@gmail.com>
2019-10-30 20:08:00 +0200

    travis: nothing to run, only tests
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- ci/script.sh
+++ ci/script.sh
@@ -16,8 +16,9 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    # no binaries, nothing to run
+    # cross run --target $TARGET
+    # cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
