ae02487f16426775da74c29b3557b4419ec0a93b
Sami Salonen <ssalonen@gmail.com>
2019-11-08 22:11:11 +0200

    CI: clean before_deploy
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,12 +17,6 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
-
-    # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
-
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
     cd $src
