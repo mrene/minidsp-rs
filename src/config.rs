2f5e55e692a548b16946ef834a79587377557b6f
Mathieu Rene <mathieu.rene@gmail.com>
2021-01-06 23:36:52 -0500

    feat(config): assemble binary blob for restoring an xml configuration

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,8 @@
 //! Utilities for dealing with xml configuration files
+use bytes::{Bytes};
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 use strong_xml::{XmlRead, XmlWrite};
 
 #[derive(Debug, Clone, XmlRead, XmlWrite, PartialEq)]
@@ -19,17 +23,86 @@ pub struct Setting {
     #[xml(child = "label")]
     pub labels: Vec<Label>,
 
-    #[xml(child = "item")]
-    pub items: Vec<Item>,
-
-    #[xml(child = "fir")]
-    pub fir: Vec<Fir>,
-
-    #[xml(child = "filter")]
-    pub filter: Vec<Filter>,
-
     #[xml(flatten_text = "link")]
     pub link: String,
+
+    #[xml(child = "item", child = "fir", child = "filter")]
+    pub items: Vec<AddressableElement>,
+}
+
+impl Setting {
+    /// Creates a blob that can be sent to the device in order to restore all settings under a
+    /// given configuration preset index
+    pub fn to_restore_blob(&self) -> Bytes {
+        // The configuration contains addresses which refer to _indices_ of floats in the dsp memory
+        // We can build the blob by iterating through everything, and writing a little endian f32
+        // at each address (which ends up being 4*index since each f32 is 4 bytes).a
+
+        let mut buf = RestoreBlob(Vec::with_capacity(8192));
+        buf.0.resize(8192, 0);
+
+        for item in &self.items {
+            match item {
+                AddressableElement::Item { addr, hex, .. } => {
+                    buf.put_slice_at(*addr, &hex.inner);
+                }
+                AddressableElement::Fir { addr, para, .. } => {
+                    if para.is_empty() {
+                        continue;
+                    }
+
+                    let mut addr = *addr;
+                    let para = &para[0];
+                    for subpara in &para.subpara {
+                        for value in &subpara.data.inner {
+                            buf.put_slice_at(addr, &value.inner);
+                            addr += 1;
+                        }
+                    }
+                }
+                AddressableElement::Filter { addr, hex, .. } => {
+                    let mut addr = *addr;
+                    for value in &hex.inner {
+                        buf.put_slice_at(addr, &value.inner);
+                        addr += 1
+                    }
+                }
+            }
+        }
+
+        Bytes::from(buf.0)
+    }
+
+    /// Sorts all elements by address
+    pub fn sort(&mut self) {
+        self.items.sort_unstable_by_key(|item| *match item {
+            AddressableElement::Item { addr, .. } => addr,
+            AddressableElement::Fir { addr, .. } => addr,
+            AddressableElement::Filter { addr, .. } => addr,
+        })
+    }
+}
+
+pub struct RestoreBlob(pub Vec<u8>);
+impl RestoreBlob {
+    pub fn put_slice_at(&mut self, at: usize, x: &[u8]) {
+        // Make sure the blob is big enough to hold the given address
+        let (start, end) = (at * 4, at * 4 + x.len());
+        self.ensure_size(end);
+        // Reverse the iterator because the config stores hex data in the opposite endiannesss
+        let splice = self.0.splice(start..end, x.iter().rev().cloned());
+        debug_assert!(splice.count() == x.len());
+    }
+    pub fn put_f32_le(&mut self, at: usize, x: f32) {
+        // Make sure the blob is big enough to hold the given address
+        self.put_slice_at(at, &x.to_le_bytes())
+    }
+
+    fn ensure_size(&mut self, size: usize) {
+        if self.0.len() < size {
+            self.0.resize(size, 0);
+        }
+    }
 }
 
 #[derive(Debug, Clone, XmlRead, XmlWrite, PartialEq)]
@@ -42,25 +115,57 @@ pub struct Label {
 }
 
 #[derive(Debug, Clone, XmlRead, XmlWrite, PartialEq)]
-#[xml(tag = "item")]
-pub struct Item {
-    #[xml(attr = "name")]
-    pub name: String,
-    #[xml(attr = "addr")]
-    pub addr: String,
-    #[xml(flatten_text = "dec")]
-    pub dec: String,
-    #[xml(flatten_text = "hex")]
-    pub hex: String,
+pub enum AddressableElement {
+    #[xml(tag = "item")]
+    Item {
+        #[xml(attr = "name")]
+        name: String,
+        #[xml(attr = "addr")]
+        addr: usize,
+        #[xml(flatten_text = "dec")]
+        dec: String,
+        #[xml(flatten_text = "hex")]
+        hex: HexString,
+    },
+    #[xml(tag = "fir")]
+    Fir {
+        #[xml(attr = "name")]
+        name: String,
+        #[xml(attr = "addr")]
+        addr: usize,
+        #[xml(child = "para")]
+        para: Vec<Para>,
+    },
+    #[xml(tag = "filter")]
+    Filter {
+        #[xml(attr = "name")]
+        name: String,
+        #[xml(attr = "addr")]
+        addr: usize,
+        #[xml(flatten_text = "freq")]
+        freq: u16,
+        #[xml(flatten_text = "q")]
+        q: f32,
+        #[xml(flatten_text = "boost")]
+        boost: f32,
+        #[xml(flatten_text = "type")]
+        typ: String,
+        #[xml(flatten_text = "bypass")]
+        bypass: u8,
+        #[xml(flatten_text = "dec")]
+        dec: CommaSeparatedList<f32>,
+        #[xml(flatten_text = "hex")]
+        hex: CommaSeparatedList<HexString>,
+    },
 }
 
 #[derive(Debug, Clone, XmlRead, XmlWrite, PartialEq)]
-#[xml(tag = "fir")]
-pub struct Fir {
-    #[xml(attr = "name")]
-    pub name: String,
-    #[xml(attr = "addr")]
-    pub addr: u16,
+#[xml(tag = "para")]
+pub struct Para {
+    #[xml(attr = "count")]
+    pub count: u16,
+    #[xml(attr = "rows")]
+    pub rows: u16,
     #[xml(child = "subpara")]
     pub subpara: Vec<Subpara>,
 }
@@ -71,40 +176,113 @@ pub struct Subpara {
     #[xml(attr = "row")]
     pub row: u8,
     #[xml(text)]
-    pub data: String,
+    pub data: CommaSeparatedList<HexString>,
 }
 
-#[derive(Debug, Clone, XmlRead, XmlWrite, PartialEq)]
-#[xml(tag = "filter")]
-pub struct Filter {
-    #[xml(attr = "name")]
-    pub row: String,
-    #[xml(attr = "addr")]
-    pub addr: String,
+/// Wrapper class holding a list of comma-separated values
+#[derive(Debug, Clone, PartialEq)]
+pub struct CommaSeparatedList<T> {
+    pub inner: Vec<T>,
+}
 
-    #[xml(flatten_text = "freq")]
-    pub freq: u16,
-    #[xml(flatten_text = "q")]
-    pub q: f32,
-    #[xml(flatten_text = "boost")]
-    pub boost: f32,
-    #[xml(flatten_text = "type")]
-    pub typ: String,
-    #[xml(flatten_text = "bypass")]
-    pub bypass: u8,
-    #[xml(flatten_text = "dec")]
-    pub dec: String,
-    #[xml(flatten_text = "hex")]
-    pub hex: String,
+impl<T> CommaSeparatedList<T> {
+    pub fn into_inner(self) -> Vec<T> {
+        self.inner
+    }
+}
+
+impl<T: FromStr> FromStr for CommaSeparatedList<T> {
+    type Err = <T as FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut vec = Vec::new();
+        for part in s.split(',') {
+            if !part.is_empty() {
+                vec.push(<T as FromStr>::from_str(part)?);
+            }
+        }
+        Ok(CommaSeparatedList { inner: vec })
+    }
+}
+
+impl<T: Display> Display for CommaSeparatedList<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.inner
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(",")
+        )
+    }
+}
+
+/// Wrapper class to support deserializing bytes using FromStr
+#[derive(Debug, Clone, PartialEq)]
+pub struct HexString {
+    pub inner: Bytes,
+}
+
+impl FromStr for HexString {
+    type Err = hex::FromHexError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 2 {
+            Ok(HexString {
+                inner: Bytes::from_static(&[0, 0, 0, 0]),
+            })
+        } else if s.len() % 2 != 0 {
+            Ok(HexString {
+                inner: Bytes::from(hex::decode("0".to_string() + s)?),
+            })
+        } else {
+            Ok(HexString {
+                inner: Bytes::from(hex::decode(s)?),
+            })
+        }
+    }
+}
+
+impl Display for HexString {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", hex::encode(self.inner.as_ref()))
+    }
 }
 
 mod test {
     use super::*;
-
+    
     #[test]
     fn test_deserialize() {
         let file = include_str!("./test_fixtures/config.xml");
-        let root = Setting::from_str(file);
-        println!("{:#?}", root);
+        let s = Setting::from_str(file).unwrap();
+        let cfg = s.to_restore_blob();
+
+        // TODO: Gen this from the logged packets
+        let fixture = include_bytes!("./test_fixtures/config-bulk.bin");
+        assert_eq!(cfg.as_ref(), fixture);
+    }
+
+    #[test]
+    fn test_comma_separated() {
+        let s = "1.1,2.2,3.3,4.4";
+        let expected: &[f32] = &[1.1, 2.2, 3.3, 4.4];
+        let parsed = CommaSeparatedList::<f32>::from_str(s).unwrap();
+        assert!(parsed.inner.iter().cloned().eq(expected.iter().cloned()));
+        assert_eq!(parsed.to_string().as_str(), s);
+
+        let s = "01020304,05060708,090a0b0c";
+        let expected: &[&[u8]] = &[
+            &[0x01, 0x02, 0x03, 0x04],
+            &[0x05, 0x06, 0x07, 0x08],
+            &[0x09, 0x0A, 0x0B, 0x0C],
+        ];
+        let parsed = CommaSeparatedList::<HexString>::from_str(s).unwrap();
+        for (e, p) in expected.iter().zip(parsed.inner.iter()) {
+            assert!(Bytes::from(*e).eq(&p.inner));
+        }
+        assert_eq!(parsed.to_string().as_str(), s);
     }
 }
