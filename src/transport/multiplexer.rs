125bddf7b959fa383dabb0729b638bfeb01db965
Mathieu Rene <mathieu.rene@gmail.com>
2021-01-17 09:07:55 -0500

    support multiple in flight commands, add tests

--- src/transport/multiplexer.rs
+++ src/transport/multiplexer.rs
@@ -5,13 +5,17 @@
 
 use crate::{
     commands::{Commands, Responses},
-    config::RestoreBlob,
     transport::MiniDSPError,
     Result,
 };
-use futures::{channel::oneshot, future::BoxFuture, Sink, SinkExt, Stream, StreamExt};
-
-use std::{pin::Pin, sync::{Arc, Mutex}, task::Poll};
+use futures::{channel::oneshot, future::BoxFuture, Future, Sink, SinkExt, Stream, StreamExt};
+use std::{
+    collections::VecDeque,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{self, Poll},
+};
+use task::Context;
 use tokio::sync::broadcast;
 use tower::Service;
 
@@ -22,14 +26,12 @@ type PendingCommandTuple = (Commands, oneshot::Sender<Result<Responses, MiniDSPE
 pub struct Multiplexer {
     /// If applicable, the last command that was sent, and a channel towards which the response
     /// should be sent.
-    pending_command: Arc<Mutex<Option<PendingCommandTuple>>>,
+    pending_command: Arc<Mutex<VecDeque<PendingCommandTuple>>>,
 
     /// The sending side of a broadcast channel used for received events
     event_tx: Arc<Mutex<Option<broadcast::Sender<Responses>>>>,
 
-    // Inner struct wrapping the device handle, ensuring only one sender exists simultaneously
-    // The Arc is used to be able to hold a lock guard as 'static
-    // inner: Arc<tokio::sync::Mutex<Sender>>,
+    /// Sink for sending commands
     write: tokio::sync::Mutex<BoxSink<anyhow::Error>>,
 }
 
@@ -37,11 +39,12 @@ impl Multiplexer {
     pub fn new(rx: BoxStream, tx: BoxSink<anyhow::Error>) -> Arc<Self> {
         let (recv_send, _) = broadcast::channel::<Responses>(10);
         let transport = Arc::new(Self {
-            pending_command: Arc::new(Mutex::new(None)),
+            pending_command: Arc::new(Mutex::new(VecDeque::new())),
             event_tx: Arc::new(Mutex::new(Some(recv_send.clone()))),
             write: tokio::sync::Mutex::new(tx),
         });
 
+        // Spawn the receive task
         {
             let transport = transport.clone();
             let receiver_tx = transport.event_tx.clone();
@@ -55,23 +58,28 @@ impl Multiplexer {
         transport
     }
 
-    pub async fn roundtrip(self: Arc<Self>, cmd: Commands) -> Result<Responses, MiniDSPError> {
-        let (tx, rx) = oneshot::channel();
-        {
-            let mut pending_command = self.pending_command.lock().unwrap();
-            if pending_command.is_some() {
-                // There is already an active command, this should not happen,
-                tx.send(Err(MiniDSPError::ConcurencyError)).unwrap();
-            } else {
-                pending_command.replace((cmd.clone(), tx));
-            }
-            drop(pending_command);
-        }
+    pub fn roundtrip(
+        self: &Arc<Self>,
+        cmd: Commands,
+    ) -> impl Future<Output = Result<Responses, MiniDSPError>> {
+        let this = self.clone();
+        async move {
+            let rx = {
+                let (tx, rx) = oneshot::channel();
+                let mut pending_command = this.pending_command.lock().unwrap();
+                pending_command.push_back((cmd.clone(), tx));
+                rx
+            };
 
-        self.write.lock().await.send(cmd).await?;
-        rx.await.map_err(|_| MiniDSPError::TransportClosed)?
+            let mut writer = this.write.lock().await;
+            writer.send(cmd).await?;
+
+            rx.await.map_err(|_| MiniDSPError::TransportClosed)?
+        }
     }
 
+    /// Receives responses from the transport, dispatches responses to the first pending command if it matches, else
+    /// pushed it to the events broadcast.
     async fn recv_loop(
         self: Arc<Self>,
         sender: broadcast::Sender<Responses>,
@@ -88,14 +96,14 @@ impl Multiplexer {
 
             {
                 let mut pending_cmd = self.pending_command.lock().unwrap();
-                let matches = if let Some((cmd, _)) = pending_cmd.as_ref() {
+                let matches = if let Some((cmd, _)) = pending_cmd.front() {
                     cmd.matches_response(&data)
                 } else {
                     false
                 };
 
                 if matches {
-                    let (_, channel) = pending_cmd.take().unwrap();
+                    let (_, channel) = pending_cmd.pop_front().unwrap();
                     let _ = channel.send(Ok(data)); // Discard errors because it means the caller gave up
                     continue;
                 }
@@ -123,14 +131,48 @@ impl Service<Commands> for Arc<Multiplexer> {
     type Error = MiniDSPError;
     type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
-    fn poll_ready(
-        &mut self,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), Self::Error>> {
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, req: Commands) -> Self::Future {
-        Box::pin(self.clone().roundtrip(req))
+        Box::pin(self.roundtrip(req))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::commands::BytesWrap;
+    use super::*;
+    use bytes::Bytes;
+    use futures::channel::mpsc;
+    use futures_util::join;
+
+    #[tokio::test]
+    async fn test_golden_path() {
+        let (sink_tx, mut sink_rx) = mpsc::channel::<Commands>(10);
+        let (mut stream_tx, stream_rx) = mpsc::channel::<Responses>(10);
+
+        let mplex = Multiplexer::new(Box::pin(stream_rx), Box::pin(sink_tx.sink_err_into()));
+        let resp1 = mplex.roundtrip(Commands::SetMute { value: true });
+        let resp2 = mplex.roundtrip(Commands::ReadHardwareId);
+        let answer = async move {
+            let cmd = sink_rx.next().await.unwrap();
+            assert!(matches!(cmd, Commands::SetMute { .. }));
+            stream_tx.send(Responses::Ack).await.unwrap();
+
+            let cmd = sink_rx.next().await.unwrap();
+            assert!(matches!(cmd, Commands::ReadHardwareId { .. }));
+            stream_tx
+                .send(Responses::HardwareId {
+                    payload: BytesWrap(Bytes::from_static(b"allo")),
+                })
+                .await
+                .unwrap();
+        };
+
+        let (resp1, resp2, _) = join!(resp1, resp2, answer);
+        assert!(matches!(resp1.unwrap(), Responses::Ack));
+        assert!(matches!(resp2.unwrap(), Responses::HardwareId{..}));
     }
 }
