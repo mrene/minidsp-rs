7d42d8e3188e98bf713f15e6cd8eb823af37c9ca
Mathieu Rene <mathieu.rene@gmail.com>
2021-05-17 23:23:19 -0400

    fix doc build

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+cd "$(dirname "$0")"
+
 if [ ! -f './mdbook' ]; then
     curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.8/mdbook-v0.4.8-x86_64-unknown-linux-gnu.tar.gz | tar zxv
 fi
