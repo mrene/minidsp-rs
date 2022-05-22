ca5549d0548565aa801d1318c8ee1bfd9c422e62
Mathieu Rene <mathieu.rene@gmail.com>
2022-05-21 23:50:15 -0400

    fix(rpi): downgrade glibc version to 2.28

--- scripts/build-pi-armv6hf.sh
+++ scripts/build-pi-armv6hf.sh
@@ -9,7 +9,7 @@ set -eux
 HIDAPI_VER="0.8.0~rc1+git20140818.d17db57+dfsg-2"
 LIBUSB_VER="1.0.22-2"
 LIBUDEV_VER="241-7~deb10u8+rpi1"
-LIBC_VER="2.33-7+rpi1"
+LIBC_VER="2.28-10+rpi1+deb10u1"
 OPENSSL_VER="1.1.1o-1"
 DEPS=( \
   "http://mirrordirector.raspbian.org/raspbian/pool/main/h/hidapi/libhidapi-libusb0_${HIDAPI_VER}_armhf.deb" \
@@ -68,8 +68,8 @@ export CARGO_TARGET_arm_unknown_linux_gnueabihf_LINKER="gcc-sysroot"
 export CFLAGS="-std=c99"
 
 # Overwrite libc and libpthread with the new ones since the sysroot ones are outdated
-cp $SYSROOT/lib/arm-linux-gnueabihf/libc-2.33.so $SYSROOT/lib/libc.so.6
-cp $SYSROOT/lib/arm-linux-gnueabihf/libpthread-2.33.so $SYSROOT/lib/libpthread.so.0
+cp $SYSROOT/lib/arm-linux-gnueabihf/libc-2.28.so $SYSROOT/lib/libc.so.6
+cp $SYSROOT/lib/arm-linux-gnueabihf/libpthread-2.28.so $SYSROOT/lib/libpthread.so.0
 
 CMD=$1
 shift
