d52d426a66124ddf7e11c41726dd3958135a8451
Mathieu Rene <mathieu.rene@gmail.com>
2023-02-12 18:32:29 -0500

    fix(nix): point nixpkgs to nixos-22.11 since it doesn't have the CC wrapper rpath bug

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A control interface for some MiniDSP products";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
